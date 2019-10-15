776beb99d7f63c775b426af3b10c07737ed7d80d
kkuyu <devils67@naver.com>
2019-10-15 16:15:13 +0900

    useInput modify : multiple check validator

--- useInput/README.md
+++ useInput/README.md
@@ -4,11 +4,14 @@
 import useInput from "@custom_react_hook/useInput";
 const App = () => {
 	const maxLength = value => value.length <= 10;
-	const name = useInput("Hello", maxLength);
+	const characterCheck = value => !value.includes("@");
+	const name = useInput("John Doe", maxLength); // One validator
+	const nickname = useInput("John", maxLength, characterCheck); // Multiple validator
 	return (
 		<div className="App">
 			<h1>Hello</h1>
 			<input type="text" placeholder="Name" {...name} />
+			<input type="text" placeholder="Nickname" {...nickname} />
 		</div>
 	);
 };
--- useInput/useInput.js
+++ useInput/useInput.js
@@ -1,11 +1,17 @@
-export const useInput = (initialValue, validator) => {
+export const useInput = (initialValue, ...validator) => {
 	const [value, setValue] = useState(initialValue);
 	const onChange = event => {
 		const { value } = event.target;
 		let willUpdate = true;
-		if (typeof validator === "function") {
-			willUpdate = validator(value);
-		}
+
+		const validatorArray = validator.length
+			? validator.filter(validator => typeof validator === "function")
+			: null;
+		const validatorResult = validatorArray.map(validator =>
+			validator(value),
+		);
+
+		willUpdate = !validatorResult.includes(false);
 		if (willUpdate) {
 			setValue(value);
 		}
