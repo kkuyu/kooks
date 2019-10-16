798e2da3ac60364cb6d76cfcbba3f1ec46a7b05b
kkuyu <devils67@naver.com>
2019-10-16 13:18:42 +0900

    useTabs modify : Add accessibility keyboard events

--- useTabs/README.md
+++ useTabs/README.md
@@ -5,21 +5,27 @@ import useTabs from "@custom_react_hook/useTabs";
 const content = [
 	{ title: "tab1", content: "tab1 content" },
 	{ title: "tab2", content: "tab2 content" },
+	{ title: "tab3", content: "tab3 content" },
+	{ title: "tab4", content: "tab4 content" },
 ];
+
 const App = () => {
-	const { currentItem, setCurrentIndex } = useTabs(0, content);
+	const { tabButtonRef, currentIndex, onClick, onKeyDown } = useTabs(0, content);
 	return (
 		<div>
-			{content.map((content, index) => (
-				<button
-					type="button"
-					key={index}
-					onClick={() => setCurrentIndex(index)}
-				>
-					{content.title}
-				</button>
-			))}
-			<div>{currentItem.content}</div>
+			<div ref={tabButtonRef}>
+				{content.map((content, index) => (
+					<button
+						type="button"
+						key={index}
+						onClick={() => onClick(index)}
+						onKeyDown={event => onKeyDown(event, index)}
+					>
+						{content.title}
+					</button>
+				))}
+			</div>
+			<div>{content[currentIndex].content}</div>
 		</div>
 	);
 };
--- useTabs/useTabs.js
+++ useTabs/useTabs.js
@@ -2,9 +2,41 @@ export const useTabs = (initialTab, allTabs) => {
 	if (!allTabs || !Array.isArray(allTabs)) {
 		return false;
 	}
+
 	const [currentIndex, setCurrentIndex] = useState(initialTab);
-	return {
-		currentItem: allTabs[currentIndex],
-		setCurrentIndex,
+	const tabButtonRef = useRef(null);
+
+	const activeIndex = (index, isFocus=false) => {
+		const tabButtonArray = tabButtonRef.current.querySelectorAll("button");
+		setCurrentIndex(index);
+		if (isFocus) {
+			tabButtonArray[index].focus();
+		}
 	};
+
+	const onClick = (index) => {
+		activeIndex(index, false)
+	}
+
+	const onKeyDown = (event, index) => {
+		if (event.keyCode === 36) {
+			// home key
+			activeIndex(0, true);
+		} else if (event.keyCode === 35) {
+			// end key
+			activeIndex(allTabs.length - 1, true);
+		} else if (event.keyCode === 37) {
+			// left key
+			index !== 0
+				? activeIndex(index - 1, true)
+				: activeIndex(allTabs.length - 1, true);
+		} else if (event.keyCode === 39) {
+			// right key
+			index !== allTabs.length - 1
+				? activeIndex(index + 1, true)
+				: activeIndex(0, true);
+		}
+	};
+
+	return { tabButtonRef, currentIndex, onClick, onKeyDown };
 };
